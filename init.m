3c9853b301c090788bf69b2b83738ad1add1fbdd
Jonathan Grizou <jonathan.grizou@inria.fr>
2014-09-18 22:26:09 -0500

    adapt init to new repo organisation

--- init.m
+++ init.m
@@ -1,4 +1,5 @@
 [pathstr, ~, ~] = fileparts(mfilename('fullpath'));
 addpath(genpath(pathstr));
-addpath(genpath(fullfile(pathstr, '../lfuf')));
+addpath(genpath(fullfile(pathstr, '../lfui')));
+addpath(genpath(fullfile(pathstr, '../matlab_tools')));
 clear 'pathstr'
